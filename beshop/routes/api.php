5fd36f6e810caea387013454b2c50e53c4dbb5e7
liuliu <liu.liu@hiskio.com>
2021-06-18 22:38:20 +0800

    feat: 後端

--- beshop/routes/api.php
+++ beshop/routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/items', 'App\Http\Controllers\ItemsController@index');
+Route::get('/colors', 'App\Http\Controllers\ItemsController@colors');
+
+Route::get('/categories', 'App\Http\Controllers\CategoriesController@index');
